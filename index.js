a51fd0915b80095ef675dcc2b615e9fad2e2fb18
aelmardhi <aelmardhi@gmail.com>
2020-08-20 00:54:01 +0200

    editing ytdl

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const authRoute = require('./routes/auth');
 const messageRoute = require('./routes/messages');
+const ytdlRoute = require('./routes/ytdl');
 
 const cloudinary = require('cloudinary');
 
@@ -47,6 +48,7 @@ app.use((req,res,next) => {
 });
 app.use('/api/user', authRoute);
 app.use('/api/messages', messageRoute);
+app.use('/api/ytdl', ytdlRoute);
 
 app.post('/api/download', async (req,res)=>{
          try{
--- routes/ytdl.js
+++ routes/ytdl.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const router = require('express').Router();
-// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
-// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
-// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
+
 
 
 
@@ -11,6 +9,7 @@ router.post('/info', async (req,res)=> {
     if(!(req && req.body && req.body.id)){
         res.status(400).send('request should have body with id');
     }
+    console.log(req.body);
     let info = await ytdl.getInfo(req.body.id);
     let retInfo = {
         'id': info.videoDetails.videoId,
@@ -27,7 +26,7 @@ router.post('/info', async (req,res)=> {
     res.status(200).json(retInfo);
 });
 
-router.post('downlpad', async(req, res) => {
+router.post('/downlpad', async(req, res) => {
     const fn = req.body.title + '.'+req.body.container;
     const fl = __dirname+'/public/downloads/'+fn;
     ytdl(req.body.id,{"quality": req.body.itag})
