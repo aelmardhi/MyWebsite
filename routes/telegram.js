5c78a37f359e7fe6ab5e75158e33721dfbb8b6bc
aelmardhi <aelmardhi@gmail.com>
2021-01-22 23:24:28 +0200

    tele dow

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,7 +1,17 @@
+const { urlencoded } = require('body-parser');
+
 const router = require('express').Router();
+const urlModule = require('url');
+const https = require('https');
+
 
 router.post('/update',async (req,res)=>{
-    console.log(req.body);
+    const url = urlModule.parse(req.body.message.text);
+    if(!url.pathname){
+        res.send('ok');
+        return;
+    }
+    https.post('../../download/',{'url':req.body.message.text});
     res.json({
         'method':'sendMessage',
         'chat_id':req.body.message.chat.id,
