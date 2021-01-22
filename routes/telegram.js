fe71bd691a52988b962b2035cc89948589400ffe
aelmardhi <aelmardhi@gmail.com>
2021-01-22 23:04:08 +0200

    tele echo

--- routes/telegram.js
+++ routes/telegram.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 
 router.post('/update',async (req,res)=>{
     console.log(req.body);
-    res.send('ok')
+    res.json({
+        'method':'sendMessage',
+        'chat_id':req.body.message.chat.id,
+        'text':req.body.message.text,
+    })
 });
 
 module.exports = router;
