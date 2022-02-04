affbb7d976e0da000e54bd7982826a005184673e
aelmardhi <aelmardhi@gmail.com>
2022-02-04 10:41:01 +0200

    w

--- utils/httpRedirect.js
+++ utils/httpRedirect.js
@@ -17,7 +17,7 @@ function httpRedirect(url){
     return new Promise(async(resolve,rej)=>{
         protocol.get(url,async res => {
             if(res.headers.location){
-                let u = await getLink(res.headers.location);
+                let u = await httpRedirect(res.headers.location);
                 resolve (u)
             }
             resolve( url);
