d4284d61f3f45097b09d5cc05a37af6179478c88
aelmardhi <aelmardhi@gmail.com>
2022-08-21 20:53:22 +0200

    prayers ToEulerAngles

--- public/prayers/degHelpers.js
+++ public/prayers/degHelpers.js
@@ -31,3 +31,26 @@ function atan2(a,b){
 function acot(a){
     return (rad2deg( Math.atan(1/a)))
 }
+
+function ToEulerAngles( q) {
+    let angles = {};
+    q = {x:q[0], y:q[1], z:q[2], w:q[3]}
+    // roll (x-axis rotation)
+    let sinr_cosp = 2 * (q.w * q.x + q.y * q.z);
+    let cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y);
+    angles.roll = rad2deg(Math.atan2(sinr_cosp, cosr_cosp));
+
+    // pitch (y-axis rotation)
+    let sinp = 2 * (q.w * q.y - q.z * q.x);
+    if (Math.abs(sinp) >= 1)
+        angles.pitch = rad2deg(Math.abs(Math.PI / 2)* Math.sign( sinp)); // use 90 degrees if out of range
+    else
+        angles.pitch = rad2deg(Math.asin(sinp));
+
+    // yaw (z-axis rotation)
+    let siny_cosp = 2 * (q.w * q.z + q.x * q.y);
+    let cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
+    angles.yaw = rad2deg(Math.atan2(siny_cosp, cosy_cosp));
+
+    return angles;
+}
