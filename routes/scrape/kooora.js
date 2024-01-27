7c0186693c9f07b7126967d547a77c878f9ba146
Abdalla Elmardi <aelmardhi@gmail.com>
2024-01-27 13:04:22 +0200

    Update kooora.js

--- routes/scrape/kooora.js
+++ routes/scrape/kooora.js
@@ -61,8 +61,8 @@ async function LoadNews (browser,timezone){
 async function updateNews(page,timezone,urlQuery){
     try{
         page.emulateTimezone(timezone)
-        await page.goto(baseURL+'?'+urlQuery,{timeout:300000, waitUntil:"domcontentloaded"});
-        return await page.$eval('.content .articlePage .articleBody', el => {
+        await page.goto(baseURL+urlQuery,{timeout:300000, waitUntil:"domcontentloaded"});
+        return await page.$eval('#content .articlePage .articleBody', el => {
             el.querySelectorAll('div').forEach(element => {
                 element.remove();
             });
