c2ed05f45c88f6715b0fc4c0e7aca43fe2fe72a7
Abdalla Elmardi <aelmardhi@gmail.com>
2024-07-16 14:32:36 +0200

    Fix thpo

--- sockets.js
+++ sockets.js
@@ -22,26 +22,7 @@ io.on("connection", (socket) => {
 });
 io.on('log',(id, msg)=>{
   io.broadcast(msg);
-})io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).emit("user-connected", userId);
-    socket.on("disconnect",()=>{
-	socket.to(roomId).emit("user-disconnected", userId);
-    })	
-  });
-  socket.on('msg',(msg)=>{
-
-    socket.rooms.forEach(r => socket.to(r).emit('msg',msg));
-  })
-  socket.on('close-call',(msg)=>{
-
-    socket.rooms.forEach(r => socket.to(r).emit('close-call',msg));
-  })
-});
-io.on('log',(id, msg)=>{
-  io.broadcast(msg);
-});
+})
 };
 
 module.exports = addSocket;
