3230e85ac2ed2a6d1a0c8ee36af9f0970577c557
Abdalla Elmardi <aelmardhi@gmail.com>
2024-07-27 10:40:32 +0200

    Update kooora.js matches url changed

--- routes/scrape/kooora.js
+++ routes/scrape/kooora.js
@@ -89,7 +89,7 @@ async function getKooraHome(browser,timezone){
         await page.waitForSelector('.liveMatches .flickity-slider');
      
     const matches = await page.$eval('.liveMatches .flickity-slider', el => {
-        return el.innerHTML;
+        
         function parseTDTeam(td){
             return {
                 img: td.querySelector('img')?.getAttribute('src'),
@@ -108,7 +108,7 @@ async function getKooraHome(browser,timezone){
         }
         function parseScoreRow (r){
             return{
-                url: r.getAttribute('onclick').replace("location='",'').replace("';",''),
+                url: '?m='+r.getAttribute('onclick'),
                 home:  parseTDTeam(r.childNodes[1]),
                 competition: parseTDCompetition(r),
                 away: parseTDTeam(r.childNodes[2]),
@@ -127,7 +127,7 @@ async function getKooraHome(browser,timezone){
         return  r
     
     }).catch(logError('Home::Matches'))
-console.log(matches);
+
     await page.waitForSelector('.newsList.topNews');    
     let featurednews = await page.$eval('.newsList.topNews', el => {
         let news = []
