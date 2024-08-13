c12f6b510b89c2865c5a06faaf5c8aeb99aae6b8
Abdalla Elmardi <aelmardhi@gmail.com>
2024-08-13 10:42:25 +0200

    Update notify.js

--- utils/notify.js
+++ utils/notify.js
@@ -1,2 +1,16 @@
 const {Subscribtion} = require('../models/subscribtion');
+const webPush = require('web-push');
 
+const notifyAll = (payload) => {
+  return Subscribtion.find()
+    .then(subscriptions =>
+      Promise.all(subscribtions.map(subscription =>
+        webPush.sendNotification(
+          JSON.parse(subscription.subscribtion),
+          JSON.stringify( payload )|| '{title:"",body:""}'
+                                 )
+                                   )
+            ));                     
+}    
+
+module.exports.notifyAll = notifyAll;
