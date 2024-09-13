b52deacac136cbaaff7459b8694245892066deca
Abdalla Elmardi <aelmardhi@gmail.com>
2024-09-13 15:42:20 +0200

    Update app.js security updates
    Use helmet and 
    disable 'x-powered-by'

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const fs = require('fs');
 const urlModule = require('url');
 const app = express();
@@ -21,6 +22,8 @@ const blogRoute = require('./routes/blog')
 const uploadImageRoute = require('./routes/uploadImage')
 const scrapeRoute = require('./routes/scrape')
 
+app.disable('x-powered-by');
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
